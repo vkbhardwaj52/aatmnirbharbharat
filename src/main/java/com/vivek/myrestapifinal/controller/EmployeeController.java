ca1a79e9e45d23deb245e09bb2ea48e1bff6b569
vkbhardwaj52 <60341667+vkbhardwaj52@users.noreply.github.com>
2020-06-05 20:42:04 +0530

    Update EmployeeController.java

--- src/main/java/com/vivek/myrestapifinal/controller/EmployeeController.java
+++ src/main/java/com/vivek/myrestapifinal/controller/EmployeeController.java
@@ -1,21 +1,99 @@
 package com.vivek.myrestapifinal.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.*;
+import java.util.List;
 
-@Controller
-@SpringBootApplication
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vivek.myrestapifinal.dao.EmployeeDAO;
+import com.vivek.myrestapifinal.model.Employee;
+
+@RestController
+@RequestMapping("/company")
 public class EmployeeController {
+	
+	@Autowired
+	EmployeeDAO employeeDAO;
+	
+	
+	//To save an employee
+	@PostMapping("/employees")
+	public Employee createEmployee(@Valid @RequestBody Employee emp)
+	{
+		return employeeDAO.save(emp);
+	}
+	
+	//To get all employee
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees()
+	{
+		return employeeDAO.findAll();
+	}
+	
+	//To get all employee
+	@GetMapping("/test")
+	public String getTestData()
+	{
+		return "Test Success";
+	}
+	
+	//To get employee by id
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable(value="id") Long empid){
+		
+		Employee emp=employeeDAO.findOne(empid);
+		
+		if(emp==null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok().body(emp);
+		
+	}
+	
+	
+	/*update an employee by empid*/
+	@PutMapping("/employees/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable(value="id") Long empid,@Valid @RequestBody Employee empDetails){
+		
+		Employee emp=employeeDAO.findOne(empid);
+		if(emp==null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		emp.setName(empDetails.getName());
+		emp.setDesignation(empDetails.getDesignation());
+		emp.setExpertise(empDetails.getExpertise());
+		
+		Employee updateEmployee=employeeDAO.save(emp);
+		return ResponseEntity.ok().body(updateEmployee);
+		
+		
+		
+	}
+	
+	/*Delete an employee*/
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<Employee> deleteEmployee(@PathVariable(value="id") Long empid){
+		
+		Employee emp=employeeDAO.findOne(empid);
+		if(emp==null) {
+			return ResponseEntity.notFound().build();
+		}
+		employeeDAO.delete(emp);
+		
+		return ResponseEntity.ok().build();
+		
+		
+	}
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-      return "Hello World!";
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(EmployeeController.class, args);
-    }
 }
