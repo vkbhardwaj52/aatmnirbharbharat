c7e1ca51bd4d2edc36c81402c90d25b48a5a4870
vkbhardwaj52 <60341667+vkbhardwaj52@users.noreply.github.com>
2020-06-06 11:54:22 +0530

    Delete CategoriesRepository.java

--- src/main/java/com/vivek/myrestapifinal/repository/CategoriesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vivek.myrestapifinal.repository;
-
-import com.vivek.myrestapifinal.model.Categories;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriesRepository extends JpaRepository<Categories,Long>{
-
-}
