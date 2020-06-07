bb621b1ee4834a048f44a644a0b965d82272dbfe
vkbhardwaj52 <60341667+vkbhardwaj52@users.noreply.github.com>
2020-06-07 18:29:13 +0530

    Delete SubCategoriesRepository.java

--- src/main/java/com/vivek/myrestapifinal/myrestapifinal/repository/SubCategoriesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vivek.myrestapifinal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vivek.myrestapifinal.model.SubCategories;
-
-public interface SubCategoriesRepository extends JpaRepository<SubCategories,Long>{
-
-}
