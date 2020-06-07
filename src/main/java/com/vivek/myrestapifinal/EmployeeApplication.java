01e3d93d64215012c8c53078758fac2fd929632d
vkbhardwaj52 <60341667+vkbhardwaj52@users.noreply.github.com>
2020-06-07 10:16:31 +0530

    Update EmployeeApplication.java

--- src/main/java/com/vivek/myrestapifinal/EmployeeApplication.java
+++ src/main/java/com/vivek/myrestapifinal/EmployeeApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@SpringBootApplication
-@SpringBootApplication(scanBasePackages = {"com.vivek.myrestfinal"})
+@SpringBootApplication
 @EnableJpaAuditing
 public class EmployeeApplication {
 	
