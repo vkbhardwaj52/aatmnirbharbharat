47c50e0b1ffd59d44717aec4f2c566cdc528da87
vkbhardwaj52 <60341667+vkbhardwaj52@users.noreply.github.com>
2020-06-07 18:59:23 +0530

    Delete ItemProfileRepository.java

--- src/main/java/com/vivek/myrestapifinal/repository/ItemProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vivek.myrestapifinal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vivek.myrestapifinal.model.ItemProfile;
-
-public interface ItemProfileRepository extends JpaRepository<ItemProfile,Long>{
-
-}
