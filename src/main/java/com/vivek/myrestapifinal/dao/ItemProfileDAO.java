07f77cb1d7ca18a0256530579028b5f5a189e107
vkbhardwaj52 <60341667+vkbhardwaj52@users.noreply.github.com>
2020-06-07 23:47:41 +0530

    Update ItemProfileDAO.java

--- src/main/java/com/vivek/myrestapifinal/dao/ItemProfileDAO.java
+++ src/main/java/com/vivek/myrestapifinal/dao/ItemProfileDAO.java
@@ -2,6 +2,11 @@ package com.vivek.myrestapifinal.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +15,9 @@ import com.vivek.myrestapifinal.repository.ItemProfileRepository;
 
 @Service
 public class ItemProfileDAO {
+	
+	private static EntityManagerFactory entityManagerFactory =
+	          Persistence.createEntityManagerFactory("example-unit");
 	
 	@Autowired
 	ItemProfileRepository itemProfileRepository;
@@ -28,6 +36,16 @@ public class ItemProfileDAO {
 		return itemProfileRepository.findAll();
 	}
 	
+	//Search all item
+	public List<ItemProfile> findAllWithConditions(String subCategoriesId)
+	{
+		EntityManager em = entityManagerFactory.createEntityManager();
+	      Query query = em.createQuery("SELECT e FROM item_profile e WHERE e.sub_categories_id = :subCategoriesId");
+	      query.setParameter("subCategoriesId", subCategoriesId);
+	      List<ItemProfile> resultList = query.getResultList();
+	      em.close();
+	      return resultList;
+	}
 	
 	//Get an item
 	public ItemProfile findOne(Long itemId)
