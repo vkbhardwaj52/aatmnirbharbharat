ae19788d659942c0d06d1880bde3d296094a7895
vkbhardwaj52 <60341667+vkbhardwaj52@users.noreply.github.com>
2020-06-07 11:13:13 +0530

    Delete EmployeeRepository.java

--- src/main/java/com/vivek/myrestapifinal/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vivek.myrestapifinal.repository;
-
-import com.vivek.myrestapifinal.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long>{
-
-}
