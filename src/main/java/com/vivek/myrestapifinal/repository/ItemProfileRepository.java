d2c70959f0d78b8621b97e72a8df2b3b50ed3039
vkbhardwaj52 <60341667+vkbhardwaj52@users.noreply.github.com>
2020-06-07 18:23:21 +0530

    Add files via upload

---
+++ src/main/java/com/vivek/myrestapifinal/repository/ItemProfileRepository.java
@@ -0,0 +1,9 @@
+package com.vivek.myrestapifinal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vivek.myrestapifinal.model.ItemProfile;
+
+public interface ItemProfileRepository extends JpaRepository<ItemProfile,Long>{
+
+}
