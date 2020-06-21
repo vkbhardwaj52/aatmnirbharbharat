299a2e887ebb9c4f7b7aa8dc905941ede954e6c6
vkbhardwaj52 <60341667+vkbhardwaj52@users.noreply.github.com>
2020-06-21 09:54:06 +0530

    Add files via upload

---
+++ src/main/java/com/vivek/myrestapifinal/repository/SubCategoriesRepository.java
@@ -0,0 +1,10 @@
+package com.vivek.myrestapifinal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vivek.myrestapifinal.model.SubCategories;
+
+public interface SubCategoriesRepository extends JpaRepository<SubCategories,Long>{
+	
+
+}
