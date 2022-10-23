1f41745f5160e27b6b6d0b516289e719f21cf8c6
vkbhardwaj52 <60341667+vkbhardwaj52@users.noreply.github.com>
2022-10-23 15:45:24 +0530

    Delete ShopRepository.java

--- src/main/java/com/vivek/myrestapifinal/repository/ShopRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vivek.myrestapifinal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vivek.myrestapifinal.model.Shop;
-
-public interface ShopRepository extends JpaRepository<Shop,Long>{
-
-}
