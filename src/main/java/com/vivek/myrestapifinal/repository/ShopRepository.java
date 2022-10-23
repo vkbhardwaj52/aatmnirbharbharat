0787eda75141cc5a0237c2a80dccd1b77037c9f0
vkbhardwaj52 <60341667+vkbhardwaj52@users.noreply.github.com>
2022-10-23 15:25:26 +0530

    Add files via upload

---
+++ src/main/java/com/vivek/myrestapifinal/repository/ShopRepository.java
@@ -0,0 +1,9 @@
+package com.vivek.myrestapifinal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vivek.myrestapifinal.model.Shop;
+
+public interface ShopRepository extends JpaRepository<Shop,Long>{
+
+}
